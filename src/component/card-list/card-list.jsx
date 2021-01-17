77aa75c9142f78160a490f8112c538c545f96f18
vibek44 <kshetrivibek@gmail.com>
2021-01-17 18:01:41 +0200

    modiefied card-list

--- src/component/card-list/card-list.jsx
+++ src/component/card-list/card-list.jsx
@@ -1,7 +1,6 @@
 import './card-list.styles.css';
+import {Card} from '../card/card.jsx';
 
-export const Cardlist=(props)=>
-<div className='card-list'>
-   { props.monsters.map((monster)=><h1>{monster.name}</h1>)
-   }
+export const Cardlist=(props)=><div className='card-list'>
+    {props.monsters.map((monster)=><Card key={monster.id} monster={monster} />)}
 </div>
