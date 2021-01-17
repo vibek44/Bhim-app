aface81640d1da5e199aff8b52a02e232a944bf7
vibek44 <kshetrivibek@gmail.com>
2021-01-17 18:04:45 +0200

    adding email element

--- src/component/card/card.jsx
+++ src/component/card/card.jsx
@@ -4,4 +4,5 @@ export const Card=(props)=>
 <div className='card-container'>
    <img alt='roboimage' src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}/>
     <h1>{props.monster.name}</h1>
+    <p>{props.monster.email}</p>
     </div>
