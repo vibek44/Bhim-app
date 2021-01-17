64cb99c846bbbe1e88b9ca27be84e97abab2742f
vibek44 <kshetrivibek@gmail.com>
2021-01-17 17:54:10 +0200

    adding card component

---
+++ src/component/card/card.jsx
@@ -0,0 +1,7 @@
+import './card.styles.css';
+
+export const Card=(props)=>
+<div className='card-container'>
+   <img alt='roboimage' src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}/>
+    <h1>{props.monster.name}</h1>
+    </div>
